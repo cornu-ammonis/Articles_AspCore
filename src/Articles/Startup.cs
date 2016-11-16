6098dc5ee80ff451a7237b515faac53ce4b0bff0
cornu-ammonis <andrewjones232@gmail.com>
2016-11-15 22:21:18 -0500

    removed user secrets

--- src/Articles/Startup.cs
+++ src/Articles/Startup.cs
@@ -31,7 +31,8 @@ namespace Articles
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+               
+               // builder.AddUserSecrets();
 
                 // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
                 builder.AddApplicationInsightsSettings(developerMode: true);
