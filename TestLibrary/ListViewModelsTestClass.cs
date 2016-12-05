ff3bbc1cd925000dda056037fcdbce6d813f16b0
cornu-ammonis <andrewjones232@gmail.com>
2016-12-04 21:10:43 -0500

    2 unit tests for SubscribedListViewModel
    verifies that SubscribedListViewModel constructor retrieves post list
    and post count from repository

--- TestLibrary/ListViewModelsTestClass.cs
+++ TestLibrary/ListViewModelsTestClass.cs
@@ -373,6 +373,60 @@ namespace TestLibrary
             Assert.That(searchListViewModel.TotalPosts, Is.EqualTo(postList.Count));
         }
 
+        [TestCase]
+        public void SubscribedListVIewModelRetrievesPostListFromRepository()
+        {
+            var mockRepository = new Mock<IBlogRepository>();
+            Post post1 = new Post();
+            Post post2 = new Post();
+            Post post3 = new Post();
+            Post post4 = new Post();
+
+            Category testCat1 = new Category();
+            Category testCat2 = new Category();
+            post1.Category = testCat1;
+            post2.Category = testCat2;
+            post3.Category = testCat1;
+            post4.Category = testCat2;
+
+            List<Post> postList = new List<Post>();
+            postList.Add(post1);
+            postList.Add(post2);
+            postList.Add(post3);
+            postList.Add(post4);
+
+            mockRepository.Setup(r => r.SubscribedPostsForUser("testUser", 0, 10)).Returns(postList);
+            ListViewModel subscribedListViewModel = new SubscribedListViewModel(mockRepository.Object, 1, "testUser");
+            Assert.That(subscribedListViewModel.Posts, Is.EquivalentTo(postList));
+        }
+
+        [TestCase]
+        public void SubscribedListViewModelRetrievesPostCountFromRepository()
+        {
+            var mockRepository = new Mock<IBlogRepository>();
+            Post post1 = new Post();
+            Post post2 = new Post();
+            Post post3 = new Post();
+            Post post4 = new Post();
+
+            Category testCat1 = new Category();
+            Category testCat2 = new Category();
+            post1.Category = testCat1;
+            post2.Category = testCat2;
+            post3.Category = testCat1;
+            post4.Category = testCat2;
+
+            List<Post> postList = new List<Post>();
+            postList.Add(post1);
+            postList.Add(post2);
+            postList.Add(post3);
+            postList.Add(post4);
+
+            mockRepository.Setup(r => r.TotalSubscribedPostsForUser("testUser")).Returns(postList.Count);
+            ListViewModel subscribedListViewModel = new SubscribedListViewModel(mockRepository.Object, 1, "testUser");
+            Assert.That(subscribedListViewModel.TotalPosts, Is.EqualTo(postList.Count));
+        }
+
         /* [TestCase]
          public void PostsByCategoryTest()
          {
