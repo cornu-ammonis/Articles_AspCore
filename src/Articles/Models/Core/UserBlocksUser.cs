27eeab9aa1816dede6635fa48419a6349deff3be
cornu-ammonis <andrewjones232@gmail.com>
2017-01-23 13:16:26 -0500

    fixed property names

--- src/Articles/Models/Core/UserBlocksUser.cs
+++ src/Articles/Models/Core/UserBlocksUser.cs
@@ -8,7 +8,9 @@ namespace Articles.Models.Core
     public class UserBlocksUser
     {
         public int? userBlockingId { get; set; }
+        public BlogUser blockingUser { get; set; }
 
         public int? blockedUserId { get; set; }
+        public BlogUser userBlocked { get; set; }
     }
 }
