dbdccf09a2045e4b5df6e005ab35086d36cb4fc7
cornu-ammonis <andrewjones232@gmail.com>
2017-01-28 00:05:09 -0500

    initial message class

---
+++ src/Articles/Models/Core/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.Core
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Subject { get; set; }
+        public string Contents { get; set; }
+        public DateTime SentTime { get; set; }
+        public DateTime ModifiedTime { get; set; }
+
+        public BlogUser Sender { get; set; }
+        public BlogUser Recipient { get; set; }
+
+        public UserMessageUser userMessageUser { get; set; }
+
+
+    }
+}
