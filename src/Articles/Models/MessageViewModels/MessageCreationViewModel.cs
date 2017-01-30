9c6385b0fc4ea00bb3af78d341a109b4dc1369ae
cornu-ammonis <andrewjones232@gmail.com>
2017-01-30 14:18:43 -0500

    Message Creation view model

---
+++ src/Articles/Models/MessageViewModels/MessageCreationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.MessageViewModels
+{
+    public class MessageCreationViewModel
+    {
+        public string Subject { get; set; }
+        public string Contents { get; set; }
+        public string AuthorName { get; set; }
+        public string RecipientName { get; set; }
+
+
+        
+    }
+}
