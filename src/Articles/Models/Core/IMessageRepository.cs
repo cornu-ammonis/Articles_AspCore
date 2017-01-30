2f97cf52a9956cbfd5698bf3c15d3465d1999adb
cornu-ammonis <andrewjones232@gmail.com>
2017-01-30 13:59:39 -0500

    Message Repository

---
+++ src/Articles/Models/Core/IMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.Core
+{
+   public interface IMessageRepository
+    {
+         void SendMessage(Message message);
+
+         List<Message> RetrieveMessages(string user_name);
+
+        bool CanMessage(string sender_name, string recipient_name);
+        
+
+    }
+}
---
+++ src/Articles/Models/MessageRepository.cs
@@ -0,0 +1,47 @@
+﻿using Articles.Models.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Articles.Data;
+
+namespace Articles.Models
+{
+    public class MessageRepository : BlogRepository,  IMessageRepository 
+    {
+        public MessageRepository(ApplicationDbContext database) : base(database)
+        {
+        }
+
+        public void SendMessage(Message message)
+        {
+            
+        }
+
+        public List<Message> RetrieveMessages(string user_name)
+        {
+            return new List<Message>();
+        }
+
+        public bool CanMessage(string sender_name, string recipient_name)
+        {
+            if(CheckIfBlocked(recipient_name, sender_name)) {
+                return false;
+            }
+            else if (CheckIfPublicMessaging(recipient_name))
+            {
+                return true;
+            }
+            else if (CheckIfAuthorized(recipient_name, sender_name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
