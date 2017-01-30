6e86fd57506ea6d7d52d480af928973d601e9c3e
cornu-ammonis <andrewjones232@gmail.com>
2017-01-30 14:55:03 -0500

    styling

--- src/Articles/Startup.cs
+++ src/Articles/Startup.cs
@@ -36,7 +36,7 @@ namespace Articles
                
 
                 //commented for deployment because this throws an error if run on azure 
-              // builder.AddUserSecrets();
+               //builder.AddUserSecrets();
 
                 // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
                 builder.AddApplicationInsightsSettings(developerMode: true);
