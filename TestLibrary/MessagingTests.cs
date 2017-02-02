4996f381e50e797cf476c1072321ef2754e1e7f0
cornu-ammonis <andrewjones232@gmail.com>
2017-02-02 11:48:34 -0500

    Unit Test for blocking

---
+++ TestLibrary/MessagingTests.cs
@@ -0,0 +1,35 @@
+﻿using Articles.Models.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+
+    [TestFixture]
+    public class MessagingTests
+    {
+
+        [TestCase]
+        public void BlogUserBlockUserMethodReturnsCorrectBlockingRelationship()
+        {
+            BlogUser blockingUser = new BlogUser();
+            blockingUser.user_name = "thisUserDoesTheBlocking";
+            blockingUser.UsersThisUserBlocks = new List<UserBlocksUser>();
+
+            BlogUser blockedUser = new BlogUser();
+            blockedUser.user_name = "thisUserGetsBlocked";
+            blockedUser.UsersBlockingThisUser = new List<UserBlocksUser>();
+
+            UserBlocksUser result = blockingUser.blockUser(blockedUser);
+
+            Assert.That(result.blockingUser, Is.EqualTo(blockingUser));
+            Assert.That(result.userBlocked, Is.EqualTo(blockedUser));
+            Assert.That(blockingUser.UsersThisUserBlocks, Is.EquivalentTo(blockedUser.UsersBlockingThisUser));
+
+        }
+
+    }
+}
