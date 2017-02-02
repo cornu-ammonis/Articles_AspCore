a2164ea349032d32fde9a6b9c26ea11d747d1581
cornu-ammonis <andrewjones232@gmail.com>
2017-02-02 12:07:49 -0500

    message creation unit tests

--- TestLibrary/MessagingTests.cs
+++ TestLibrary/MessagingTests.cs
@@ -1,4 +1,7 @@
-﻿using Articles.Models.Core;
+﻿using Articles.Models;
+using Articles.Models.Core;
+using Articles.Models.MessageViewModels;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -47,6 +50,59 @@ namespace TestLibrary
             Assert.That(result.authorizingUser, Is.EqualTo(authorizingUser));
             Assert.That(result.userAuthorized, Is.EqualTo(authorizedUser));
             Assert.That(authorizingUser.UsersThisUserAuthorizes, Is.EquivalentTo(authorizedUser.UsersAuthorizingThisUser));
+        }
+
+        [TestCase]
+        public void MessageCreationAssignsViewModelPropertiesAsExpected()
+        {
+            BlogUser sender = new BlogUser();
+            sender.user_name = "thisOneSends";
+
+            BlogUser receiver = new BlogUser();
+            receiver.user_name = "thisOneReceives";
+
+            MessageCreationViewModel viewModel = new MessageCreationViewModel();
+            viewModel.AuthorName = sender.user_name;
+            viewModel.RecipientName = receiver.user_name;
+            viewModel.Contents = "test contents for message creation";
+            viewModel.Subject = "test subject for message creation";
+
+            var mockRepository = new Mock<IMessageRepository>();
+            mockRepository.Setup(r => r.RetrieveUserForMessaging(sender.user_name)).Returns(sender);
+            mockRepository.Setup(r => r.RetrieveUserForMessaging(receiver.user_name)).Returns(receiver);
+
+            Message testMessage = new Message(viewModel, mockRepository.Object);
+
+            Assert.That(testMessage.Contents, Is.EqualTo(viewModel.Contents));
+            Assert.That(testMessage.Subject, Is.EqualTo(viewModel.Subject));
+
+
+        }
+
+        [TestCase]
+        public void MessageCreationRetrievesSenderAndRecipientFromRepository()
+        {
+
+            BlogUser sender = new BlogUser();
+            sender.user_name = "thisOneSends";
+
+            BlogUser receiver = new BlogUser();
+            receiver.user_name = "thisOneReceives";
+
+            MessageCreationViewModel viewModel = new MessageCreationViewModel();
+            viewModel.AuthorName = sender.user_name;
+            viewModel.RecipientName = receiver.user_name;
+            viewModel.Contents = "test contents for message creation";
+            viewModel.Subject = "test subject for message creation";
+
+            var mockRepository = new Mock<IMessageRepository>();
+            mockRepository.Setup(r => r.RetrieveUserForMessaging(sender.user_name)).Returns(sender);
+            mockRepository.Setup(r => r.RetrieveUserForMessaging(receiver.user_name)).Returns(receiver);
+
+            Message testMessage = new Message(viewModel, mockRepository.Object);
+
+            Assert.That(testMessage.Sender, Is.EqualTo(sender));
+            Assert.That(testMessage.Recipient, Is.EqualTo(receiver));
         }
 
     }
