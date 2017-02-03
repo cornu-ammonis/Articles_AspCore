64532807619c656bdbf62a610550294fd81889e8
cornu-ammonis <andrewjones232@gmail.com>
2017-02-03 17:58:50 -0500

    UnauthorizedMessageListViewModel

---
+++ src/Articles/Models/MessageViewModels/UnauthorizedMessageListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Articles.Models.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.MessageViewModels
+{
+    public class UnauthorizedMessageListViewModel : MessageListViewModel
+    {
+
+       public UnauthorizedMessageListViewModel(IMessageRepository _messageRepository, string user_name)
+        {
+            populateMessageList(_messageRepository.RetrieveUnauthorizedMessages(user_name));
+        }
+    }
+}
