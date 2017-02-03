39682ccb92cde0b9c0001af9971a3616d5fa8418
cornu-ammonis <andrewjones232@gmail.com>
2017-02-03 17:58:39 -0500

    AUthorizedMessageListViewModel

---
+++ src/Articles/Models/MessageViewModels/AuthorizedMessageListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Articles.Models.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.MessageViewModels
+{
+    public class AuthorizedMessageListViewModel : MessageListViewModel
+    {
+        public AuthorizedMessageListViewModel(IMessageRepository repo, string user_name)
+        {
+            populateMessageList(repo.RetrieveAuthorizedMessages(user_name));
+        }
+
+    }
+}
