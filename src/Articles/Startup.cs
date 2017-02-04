81d952aa6c58bf1f166a1fd94a96989d5574c8fa
cornu-ammonis <andrewjones232@gmail.com>
2017-02-04 16:05:23 -0500

    remove seed

--- src/Articles/Startup.cs
+++ src/Articles/Startup.cs
@@ -115,7 +115,7 @@ namespace Articles
 
 
             app.UseIdentity();
-           app.ApplicationServices.GetRequiredService<ApplicationDbContext>().Seed();
+          // app.ApplicationServices.GetRequiredService<ApplicationDbContext>().Seed();
 
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
