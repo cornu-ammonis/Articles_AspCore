e9b5bcd2b1c0d899bc24adeff4e45352b011847a
cornu-ammonis <andrewjones232@gmail.com>
2017-02-08 14:38:20 -0500

    MessageCreationUserSpecifiedViewModel

---
+++ src/Articles/Models/MessageViewModels/MessageCreationUserSpecifiedViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.MessageViewModels
+{
+    public class MessageCreationUserSpecifiedViewModel : MessageCreationViewModel
+    {
+        public MessageCreationUserSpecifiedViewModel(string recipientUserName)
+        {
+            RecipientName = recipientUserName;
+        }
+
+        
+    }
+}
