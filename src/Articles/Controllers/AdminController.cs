54d7617a2550cc289be26b1b2e01e52a98539ea8
cornu-ammonis <andrewjones232@gmail.com>
2017-05-08 13:18:54 -0700

    added Dependency Injection to AdminController

--- src/Articles/Controllers/AdminController.cs
+++ src/Articles/Controllers/AdminController.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Articles.Models;
 
 namespace Articles.Controllers
 {
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminController(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
