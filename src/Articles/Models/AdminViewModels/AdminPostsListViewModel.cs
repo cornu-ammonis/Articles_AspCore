7da2734cbd9413eeb5feb8abc9e9ead7e2da6355
cornu-ammonis <andrewjones232@gmail.com>
2017-05-08 13:10:30 -0700

    init AdminPostsListViewModel

---
+++ src/Articles/Models/AdminViewModels/AdminPostsListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels
+{
+    public class AdminPostsListViewModel
+    {
+        public IList<Post> posts { get; set; }
+
+
+        protected virtual void PopulatePostList(IList<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+    }
+}
