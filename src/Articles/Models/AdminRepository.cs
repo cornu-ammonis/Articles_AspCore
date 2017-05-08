c73a100edad14bd5e9105a4aa7152c2eef7a7e03
cornu-ammonis <andrewjones232@gmail.com>
2017-05-08 13:18:06 -0700

    Implemented AdminRepository

---
+++ src/Articles/Models/AdminRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models
+{
+    public class AdminRepository : IAdminRepository
+    {
+        public IList<Post> ListAllPosts()
+        {
+            return null;
+        }
+    }
+}
