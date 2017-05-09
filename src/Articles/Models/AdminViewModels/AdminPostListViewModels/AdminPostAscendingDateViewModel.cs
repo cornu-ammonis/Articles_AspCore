4d996425ec0e6e063f7fea30d1603e648b70bace
cornu-ammonis <andrewjones232@gmail.com>
2017-05-09 12:25:38 -0700

    implemented AdminPostAscendingDateViewModel

---
+++ src/Articles/Models/AdminViewModels/AdminPostListViewModels/AdminPostAscendingDateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels
+{
+    public class AdminPostAscendingDateViewModel : AdminPostsListViewModel
+    {
+        public AdminPostAscendingDateViewModel (IAdminRepository adminRepo)
+            : base(adminRepo)
+        {
+            this.PopulatePostList(adminRepo.ListAllPostsAscendingDate());
+            this.SortedBy = "DateDescending"; // used by view logic
+        }
+    }
+}
