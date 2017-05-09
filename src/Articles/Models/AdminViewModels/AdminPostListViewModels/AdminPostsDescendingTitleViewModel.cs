8f873177654e7232262379f6f9b1bf8b12c7ce67
cornu-ammonis <andrewjones232@gmail.com>
2017-05-09 12:38:44 -0700

    implemented DescendingTitle viewModel

---
+++ src/Articles/Models/AdminViewModels/AdminPostListViewModels/AdminPostsDescendingTitleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels.AdminPostListViewModels
+{
+    public class AdminPostsDescendingTitleViewModel : AdminPostsListViewModel
+    {
+        public AdminPostsDescendingTitleViewModel(IAdminRepository adminRepo)
+            :base(adminRepo)
+        {
+            this.PopulatePostList(adminRepo.ListAllPostsDescendingTitle());
+            this.SortedBy = "DescendingTitle";
+        }
+    }
+}
