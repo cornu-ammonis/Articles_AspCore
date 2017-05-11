d1ab94aad57241f435caa91db66c59e2c11cda88
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-10 21:07:36 -0700

    implemented AdminCategoriesSearchListViewModel

---
+++ src/Articles/Models/AdminViewModels/AdminCategoriesSearchListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels.AdminPostListViewModels
+{
+    public class AdminCategoriesSearchListViewModel : AdminCategoriesListViewModel
+    {
+        public AdminCategoriesSearchListViewModel(IAdminRepository adminRepo, string search) 
+            :base()
+        {
+            this.PopulateCategoriesList(adminRepo.ListCategoriesForSearch(search));
+        }
+    }
+}
