d3afc7cb61c2fa332670508820ea574d3f9c868b
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-10 20:44:03 -0700

    implemented AdminCategoryListViewModel

---
+++ src/Articles/Models/AdminViewModels/AdminCategoriesListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels
+{
+    public class AdminCategoriesListViewModel
+    {
+        public IList<Category> categories { get; set; }
+
+        public AdminCategoriesListViewModel(IAdminRepository adminRepository)
+        {
+            PopulateCategoriesList(adminRepository.ListAllCategories());
+        }
+
+        // populate the viewModel's categories list
+        // @param _categories - list of categories to display
+        protected virtual void PopulateCategoriesList(IList<Category> _categories)
+        {
+            this.categories = _categories;
+        }
+    }
+}
