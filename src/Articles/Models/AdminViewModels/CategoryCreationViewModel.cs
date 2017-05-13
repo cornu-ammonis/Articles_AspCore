422310dfce30ff58eaa5483383be57be1be23900
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-13 18:43:07 -0400

    implemented CreateAndPersistCategory method

--- src/Articles/Models/AdminViewModels/CategoryCreationViewModel.cs
+++ src/Articles/Models/AdminViewModels/CategoryCreationViewModel.cs
@@ -11,5 +11,28 @@ namespace Articles.Models.AdminViewModels
         [Required]
         public string CategoryName { get; set; }
         public string Description { get; set; }
+
+
+        // returns false if a category with that name already exists
+        // note: currently creates url slug procedurally by appending the number of 
+        // categories in the database + 1 to 'cat'
+        public bool CreateAndPersistCategory(IAdminRepository adminRepo)
+        {
+            IList<Category> ExistingCategories = adminRepo.ListAllCategories();
+            if (ExistingCategories.Any(c => c.Name.Equals(CategoryName)))
+                return false;
+
+            Category toAdd = new Category()
+            {
+                Name = CategoryName,
+                Description = Description,
+
+                // automatically initializes the url slug based on the count of current categories
+                UrlSlug = "cat" + (ExistingCategories.Count + 1).ToString()
+            };
+
+            adminRepo.AddNewCategoryToDatabase(toAdd);
+            return true;
+        }
     }
 }
