c9feb95c2dcce3e763336183532cb29febe13dc0
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-13 18:23:04 -0400

    added CategoryCreationViewModel

---
+++ src/Articles/Models/AdminViewModels/CategoryCreationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels
+{
+    public class CategoryCreationViewModel
+    {
+        [Required]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
