b60bbb8f256afdc5e61f59143f186778e3660139
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-14 13:04:38 -0400

    implemented AdminUserListViewModel abstract class

---
+++ src/Articles/Models/AdminViewModels/AdminUserListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Articles.Models.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels
+{
+
+    // abstract base class for displaying list of BlogUsers
+    // PopulateUsersList will be used in constructors of derived classes to 
+    // assign users list to the property
+    public abstract class AdminUserListViewModel
+    {
+        public IList<BlogUser> Users { get; set; }
+
+
+        // @param _users -- list of users to hold in this.Users
+        protected virtual void PopulateUsersList(IList<BlogUser> _users)
+        {
+            this.Users = _users;
+        }
+
+    }
+}
