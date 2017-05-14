9e4b7cef9b54bbb9c227a057b537519bdad38856
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-14 13:11:19 -0400

    implemneted AdminUserListAlphabetical view model

---
+++ src/Articles/Models/AdminViewModels/AdminUserListViewModels/AdminUserListAlphabetical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.Models.AdminViewModels.AdminUserListViewModels
+{
+
+    // viewmodel holding lits of BlogUsers sorted alphabetically ascending
+    public class AdminUserListAlphabetical : AdminUserListViewModel
+    {
+        public AdminUserListAlphabetical(IAdminRepository adminRepo)
+        {
+            PopulateUsersList(adminRepo.ListUsersAlphabetically());
+        }
+    }
+}
