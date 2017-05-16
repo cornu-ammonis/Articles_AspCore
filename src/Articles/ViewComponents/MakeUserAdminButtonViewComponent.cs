e2879dd673d6f8456ec985d122af6d6c6939bc9f
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-16 11:36:13 -0400

    implemented MakeUserAdminButtonViewComponent pending views

---
+++ src/Articles/ViewComponents/MakeUserAdminButtonViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Articles.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.ViewComponents
+{
+    public class MakeUserAdminButtonViewComponent : ViewComponent
+    {
+        IAdminRepository _adminRepo;
+        public MakeUserAdminButtonViewComponent(IAdminRepository adminRepo)
+        {
+            _adminRepo = adminRepo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username)
+        {
+            if (await _adminRepo.CheckIfAdminAsync(username))
+                return View("MakeNotAdmin", username);
+
+            else
+                return View("MakeAdmin", username);           
+        }
+    }
+}
