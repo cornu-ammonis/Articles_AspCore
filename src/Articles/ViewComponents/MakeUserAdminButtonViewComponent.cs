89802d5e8dbcd2de629ed449b260253aa504f796
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-17 17:56:20 -0400

    modified view which displays when user isnt fully registered

--- src/Articles/ViewComponents/MakeUserAdminButtonViewComponent.cs
+++ src/Articles/ViewComponents/MakeUserAdminButtonViewComponent.cs
@@ -31,7 +31,7 @@ namespace Articles.ViewComponents
             }
             catch 
             {
-                return View("Empty");
+                return View("EmptyNoIdentity");
             }
                     
         }
