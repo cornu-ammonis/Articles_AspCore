bcc545da55124709c68cdf822e3ee612c034a7cb
cornu-ammonis <andrew.christopher.jones@emory.edu>
2017-05-17 17:58:23 -0400

    documentation for AdminButton view component try/catch

--- src/Articles/ViewComponents/MakeUserAdminButtonViewComponent.cs
+++ src/Articles/ViewComponents/MakeUserAdminButtonViewComponent.cs
@@ -21,6 +21,14 @@ namespace Articles.ViewComponents
             if (User.Identity.Name == username)
                 return View("EmptySelf");
            
+
+            // this catches an exception which arises if a "BlogUser" entry exists without a
+            // corresponding entry in the .net identity tables. 
+            // this *should* only happen in the case where the seed method creates mock users 
+            // to author posts for the purposes of developing the site. this should never happen
+            // in production, but I'll leave the try/catch so that it's clear whats happening if
+            // this does arise in production, and to handle the case where we forget and leave 
+            // some seed method users around. 
             try
             {
                 if (await _adminRepo.CheckIfAdminAsync(username))
@@ -31,6 +39,7 @@ namespace Articles.ViewComponents
             }
             catch 
             {
+                // display message that this "BlogUser" isnt fully registered
                 return View("EmptyNoIdentity");
             }
                     
